1225bfe7e150a5315c4f53c89805e37d80e71cdf
Ohi97 <ashrafohi20@gmail.com>
2022-04-05 10:22:19 +0600

    customhookcreated

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,9 +1,40 @@
-import React from 'react';
+
+
+// import reviewHook from '../Hooks/ReviewHook';
+
+import useReview from "../hook/customhook";
 
 const Review = () => {
+     const [reviews, setReviews] = useReview();
+
+    
     return (
         <div>
-            <h2>Review</h2>
+            <h2>Review: {reviews.length}</h2>
+            <div>
+                <div>
+                    {
+                        // reviews.map(review=> <ReviewHome
+                        // key={reviews.id}
+                        // review={review}></ReviewHome>)
+                    }
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/ReviewHome/ReviewHome.js
+++ src/Components/ReviewHome/ReviewHome.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./ReviewHome.css";
 
-const ReviewHome = () => {
+
+const ReviewHome = (props) => {
+    // const {name,id,comment,ratings} = props.reviews;
     return (
         <div className='ReviewHome'>
             <div className='ReviewHome-header'>
                 <h2>This is all review</h2>
+                {/* <h2>Name:{name}</h2> */}
             </div>
             <div>
                 <button>See all reviews</button>
